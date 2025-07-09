17a7ecd877dd0c92e738716c011ef958e3cd8ae4
z3lx <57017122+z3lx@users.noreply.github.com>
2025-07-09 18:52:05 -0400

    Embed UAC info into manifest

--- src/loader/Main.cpp
+++ src/loader/Main.cpp
@@ -1,5 +1,4 @@
 #include "loader/Config.hpp"
-#include "util/Concepts.hpp"
 #include "util/win/Console.hpp"
 #include "util/win/File.hpp"
 #include "util/win/Loader.hpp"
@@ -13,65 +12,18 @@
 #include <exception>
 #include <filesystem>
 #include <iostream>
-#include <string>
 #include <thread>
 #include <vector>
 
 #include <Windows.h>
-#include <shellapi.h>
-
-namespace fs = std::filesystem;
-
-namespace {
-void RequestElevation() try {
-    // Check if process is elevated
-    wil::unique_handle token {};
-    THROW_IF_WIN32_BOOL_FALSE(OpenProcessToken(
-        GetCurrentProcess(),
-        TOKEN_QUERY,
-        token.put()
-    ));
-
-    TOKEN_ELEVATION elevation {};
-    DWORD returnElevationSize {};
-    THROW_IF_WIN32_BOOL_FALSE(GetTokenInformation(
-        token.get(),
-        TokenElevation,
-        &elevation,
-        sizeof(elevation),
-        &returnElevationSize
-    ));
-
-    if (elevation.TokenIsElevated) {
-        return;
-    }
-
-    // Restart process with elevated privileges
-    const fs::path filePath = z3lx::util::GetCurrentModuleFilePath();
-    SHELLEXECUTEINFOW info {
-        .cbSize = sizeof(info),
-        .hwnd = nullptr,
-        .lpVerb = L"runas",
-        .lpFile = filePath.c_str(),
-        .nShow = SW_NORMAL
-    };
-    THROW_IF_WIN32_BOOL_FALSE(ShellExecuteExW(
-        &info
-    ));
-
-    std::exit(0);
-} CATCH_THROW_NORMALIZED()
-} // namespace
 
 int main() try {
-    RequestElevation();
-
     // Read configuration
     z3lx::loader::Config config {};
     std::vector<uint8_t> buffer {};
 
     constexpr auto configFileName = L"loader_config.json";
-    const bool configFileExists = fs::exists(configFileName);
+    const bool configFileExists = std::filesystem::exists(configFileName);
     const wil::unique_hfile configFile =
         wil::open_or_create_file(configFileName);
 
